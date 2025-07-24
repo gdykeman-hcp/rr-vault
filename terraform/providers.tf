fd84a4d7057272885cf42dd488ed0a10f4ba71b9
Gerald Dykeman <gerald.dykeman@gmail.com>
2025-07-24 09:45:51 -0400

    remove boundary requirements

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    boundary = {
-      source  = "hashicorp/boundary"
-      version = "~> 1.3"
-    }
     vault = {
       source  = "hashicorp/vault"
       version = "~> 5.1"
@@ -15,10 +11,4 @@ provider "vault" {
   address         = var.vault_address
   token           = var.vault_token
   skip_tls_verify = true
-}
-
-provider "boundary" {
-  addr                   = var.boundary_config.addr
-  auth_method_login_name = var.boundary_config.auth_method_login_name
-  auth_method_password   = var.boundary_config.auth_method_password
 }
